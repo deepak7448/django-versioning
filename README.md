bf16640eb00982c983a6cde32667d3f57c0b59e6
deepak prajapati <deepak.prajapati@voxturrlabs.com>
2025-09-18 14:15:12 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Django Version Control
+
+A lightweight version control system for Django models without third-party packages.
+
+## Installation
+
+```bash
+pip install django-version-control
---
+++ setup.py
@@ -0,0 +1,8 @@
+import os
+from setuptools import setup
+
+# Readme as long description
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme_file:
+    long_description = readme_file.read()
+
+setup()
